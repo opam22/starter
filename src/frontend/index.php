0c10663982991bad4076e68e8fe1d15efd8dc9a6
Imam Syahru Ramadhana <knightv43@gmail.com>
2019-03-24 16:19:41 +0700

    FE 1st push

---
+++ src/frontend/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+$pages = 'index';
+if (isset($_GET['p'])) {
+    $pages = $_GET['p'];
+}
+
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader);
+
+
+switch ($pages) {
+    case 'index':
+        echo $twig->render('main.twig');
+        break;
+    case 'dashboard':
+        echo $twig->render('content/main_page.twig');
+        break;
+    case 'register':
+        echo $twig->render('register.twig');
+        break;
+    default:
+        header('HTTP/1.0 404 Not Found');
+        echo 'Page Not Found 404';
+        break;
+}
